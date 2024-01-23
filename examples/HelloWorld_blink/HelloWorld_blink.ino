bbd9d5423380b913c759bd488f2df0094c665c1d
loopstick_mm <loopstick@hotmail.com>
2024-01-23 09:22:12 -0800

    cc

--- examples/HelloWorld_blink/HelloWorld_blink.ino
+++ examples/HelloWorld_blink/HelloWorld_blink.ino
@@ -2,12 +2,12 @@
   a lovely combination of HelloWorld and Blink!
 */
 
-int ledPin = 13;
+int led = 13;
 
 void setup() {
 //initialize serial communications at a 9600 baud rate
 Serial.begin(9600);
-pinMode(ledPin, OUTPUT);
+pinMode(led, OUTPUT);
 }
 
 void loop(){
@@ -15,8 +15,8 @@ void loop(){
 Serial.println("Hello, world!");
 //wait a little so we don't see an continuous stream of messages that we can't read
 delay(500);
-digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
 delay(1000);                       // wait for a little while - do nothing else
-digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
 delay(500);                       // wait for a little while - do nothing else
 }
